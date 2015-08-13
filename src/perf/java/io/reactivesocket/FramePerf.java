0155d9b6a39742057afdfba39f2d4210fa3fc265
Ben Christensen <benjchristensen@netflix.com>
2015-08-13 13:19:48 -0700

    initial JMH setup

---
+++ src/perf/java/io/reactivesocket/FramePerf.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright 2015 Netflix, Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.reactivesocket;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.infra.Blackhole;
+
+@BenchmarkMode(Mode.Throughput)
+@OutputTimeUnit(TimeUnit.SECONDS)
+public class FramePerf {
+
+	@Benchmark
+	public void encodeNextCompleteHello(Input input) throws InterruptedException {
+		Frame.from(0, FrameType.NEXT_COMPLETE, "hello");
+	}
+	
+	@State(Scope.Thread)
+	public static class Input {
+		public Blackhole bh;
+
+		@Setup
+		public void setup(Blackhole bh) {
+			this.bh = bh;
+		}
+	}
+
+}
