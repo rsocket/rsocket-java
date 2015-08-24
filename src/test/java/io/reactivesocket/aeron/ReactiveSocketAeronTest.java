25a3fdc7c137f4ac345c0062360d06541b54f26b
Robert Roeser <rroeser@netflix.com>
2015-08-24 14:26:49 -0700

    updated operator to look for the mtulength when sending a large message

--- src/test/java/io/reactivesocket/aeron/ReactiveSocketAeronTest.java
+++ src/test/java/io/reactivesocket/aeron/ReactiveSocketAeronTest.java
@@ -12,6 +12,7 @@ import rx.RxReactiveStreams;
 import uk.co.real_logic.aeron.driver.MediaDriver;
 
 import java.nio.ByteBuffer;
+import java.util.Random;
 import java.util.concurrent.CountDownLatch;
 
 /**
@@ -66,6 +67,80 @@ public class ReactiveSocketAeronTest {
             .flatMap(i -> {
                     System.out.println("pinging => " + i);
                     Payload payload = TestUtil.utf8EncodedPayload("ping =>" + i, null);
+                    return  RxReactiveStreams.toObservable(client.requestResponse(payload));
+                }
+            )
+            .subscribe(new rx.Subscriber<Payload>() {
+                @Override
+                public void onCompleted() {
+                }
+
+                @Override
+                public void onError(Throwable e) {
+                    e.printStackTrace();
+                }
+
+                @Override
+                public void onNext(Payload s) {
+                    System.out.println(s + " countdown => " + latch.getCount());
+                    latch.countDown();
+                }
+            });
+
+        latch.await();
+    }
+
+    @Test(timeout = 60000)
+    public void sendLargeMessage() throws Exception {
+
+        Random random = new Random();
+        byte[] b = new byte[1_000_000];
+        random.nextBytes(b);
+
+        ReactiveSocketAeronServer.create(new RequestHandler() {
+            @Override
+            public Publisher<Payload> handleRequestResponse(Payload payload) {
+                System.out.println("Server got => " + b.length);
+                Observable<Payload> pong = Observable.just(TestUtil.utf8EncodedPayload("pong", null));
+                return RxReactiveStreams.toPublisher(pong);
+            }
+
+            @Override
+            public Publisher<Payload> handleRequestStream(Payload payload) {
+                return null;
+            }
+
+            @Override
+            public Publisher<Payload> handleRequestSubscription(Payload payload) {
+                return null;
+            }
+
+            @Override
+            public Publisher<Void> handleFireAndForget(Payload payload) {
+                return null;
+            }
+        });
+
+        CountDownLatch latch = new CountDownLatch(2);
+
+
+        ReactivesocketAeronClient client = ReactivesocketAeronClient.create("localhost");
+
+        Observable
+            .range(1, 2)
+            .flatMap(i -> {
+                    System.out.println("pinging => " + i);
+                    Payload payload = new Payload() {
+                        @Override
+                        public ByteBuffer getData() {
+                            return ByteBuffer.wrap(b);
+                        }
+
+                        @Override
+                        public ByteBuffer getMetadata() {
+                            return null;
+                        }
+                    };
                     return  RxReactiveStreams.toObservable(client.requestResponse(payload));
                 }
             )
