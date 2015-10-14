274747c69b6017d3cb4b0cca002de50a40490c62
Robert Roeser <rroeser@netflix.com>
2015-10-14 16:01:54 -0700

    added a fire / forget example

--- reactivesocket-aeron-examples/src/main/java/io/reactivesocket/aeron/example/requestreply/Ping.java
+++ reactivesocket-aeron-examples/src/main/java/io/reactivesocket/aeron/example/requestreply/Ping.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.reactivesocket.aeron.example;
+package io.reactivesocket.aeron.example.requestreply;
 
 import io.reactivesocket.ConnectionSetupPayload;
 import io.reactivesocket.Payload;
