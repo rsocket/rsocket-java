b4d58b1478a5daead600ab56681d1112b2cda013
Ryland Degnan <rdegnan@netflix.com>
2016-04-07 10:11:09 -0700

    Upgrade gradle-reactivesocket-plugin to 1.0.5

--- src/main/java/io/reactivesocket/ReactiveSocketSocketAddressFactory.java
+++
@@ -1,8 +0,0 @@
-package io.reactivesocket;
-
-import java.net.SocketAddress;
-
-@FunctionalInterface
-public interface ReactiveSocketSocketAddressFactory<R extends ReactiveSocket> extends ReactiveSocketFactory<SocketAddress, R> {
-
-}
