cdd239be8f3e1f9d99afd180201b6ad45b2aa284
Robert Roeser <rroeser@netflix.com>
2016-04-08 14:53:31 -0700

    adding netty tcp support for rs

---
+++ reactivesocket-netty/src/main/java/io/reactivesocket/netty/tcp/client/ClientTcpDuplexConnection.java
@@ -0,0 +1,148 @@
+/**
+ * Copyright 2015 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.reactivesocket.netty.tcp.client;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
+import io.reactivesocket.DuplexConnection;
+import io.reactivesocket.Frame;
+import io.reactivesocket.rx.Completable;
+import io.reactivesocket.rx.Observable;
+import io.reactivesocket.rx.Observer;
+import org.agrona.BitUtil;
+import org.reactivestreams.Publisher;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class ClientTcpDuplexConnection implements DuplexConnection {
+    private Channel channel;
+
+    private Bootstrap bootstrap;
+
+    private final CopyOnWriteArrayList<Observer<Frame>> subjects;
+
+    private ClientTcpDuplexConnection(Channel channel, Bootstrap bootstrap, CopyOnWriteArrayList<Observer<Frame>> subjects) {
+        this.subjects  = subjects;
+        this.channel = channel;
+        this.bootstrap = bootstrap;
+    }
+
+    public static Publisher<ClientTcpDuplexConnection> create(SocketAddress socketAddress, EventLoopGroup eventLoopGroup) {
+        if (socketAddress instanceof InetSocketAddress) {
+            try {
+                return create(socketAddress, eventLoopGroup);
+            } catch (Exception e) {
+                throw new IllegalArgumentException(e.getMessage(), e);
+            }
+        } else {
+            throw new IllegalArgumentException("unknown socket address type => " + socketAddress.getClass());
+        }
+    }
+
+    public static Publisher<ClientTcpDuplexConnection> create(InetSocketAddress address, EventLoopGroup eventLoopGroup) {
+        return s -> {
+            CopyOnWriteArrayList<Observer<Frame>> subjects = new CopyOnWriteArrayList<>();
+            ReactiveSocketClientHandler clientHandler = new ReactiveSocketClientHandler(subjects);
+            Bootstrap bootstrap = new Bootstrap();
+            ChannelFuture connect = bootstrap
+                .group(eventLoopGroup)
+                .channel(NioSocketChannel.class)
+                .handler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel ch) throws Exception {
+                        ChannelPipeline p = ch.pipeline();
+                        p.addLast(
+                            new LengthFieldBasedFrameDecoder(Integer.MAX_VALUE >> 1, 0, BitUtil.SIZE_OF_INT, -1 * BitUtil.SIZE_OF_INT, 0),
+                            clientHandler
+                        );
+                    }
+                }).connect(address.getHostName(), address.getPort());
+
+            connect.addListener(connectFuture -> {
+                if (connectFuture.isSuccess()) {
+                    final Channel ch = connect.channel();
+                    s.onNext(new ClientTcpDuplexConnection(ch, bootstrap, subjects));
+                    s.onComplete();
+                } else {
+                    s.onError(connectFuture.cause());
+                }
+            });
+        };
+    }
+
+    @Override
+    public final Observable<Frame> getInput() {
+        return o -> {
+            o.onSubscribe(() -> subjects.removeIf(s -> s == o));
+            subjects.add(o);
+        };
+    }
+
+    @Override
+    public void addOutput(Publisher<Frame> o, Completable callback) {
+        o.subscribe(new Subscriber<Frame>() {
+            @Override
+            public void onSubscribe(Subscription s) {
+                s.request(Long.MAX_VALUE);
+            }
+
+            @Override
+            public void onNext(Frame frame) {
+                try {
+                    ByteBuf byteBuf = Unpooled.wrappedBuffer(frame.getByteBuffer());
+                    ChannelFuture channelFuture = channel.writeAndFlush(byteBuf);
+                    channelFuture.addListener(future -> {
+                        Throwable cause = future.cause();
+                        if (cause != null) {
+                            callback.error(cause);
+                        }
+                    });
+                } catch (Throwable t) {
+                    onError(t);
+                }
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                callback.error(t);
+            }
+
+            @Override
+            public void onComplete() {
+                callback.success();
+            }
+        });
+    }
+
+    @Override
+    public void close() throws IOException {
+        channel.close();
+    }
+}
--- reactivesocket-netty/src/test/java/io/reactivesocket/netty/TestUtil.java
+++ reactivesocket-netty/src/test/java/io/reactivesocket/netty/TestUtil.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.reactivesocket.netty.websocket;
+package io.reactivesocket.netty;
 
 import io.netty.buffer.ByteBuf;
 import io.reactivesocket.Frame;
