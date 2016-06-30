f5d42f9be86da844dbb30f569565d6c8f5bf9a5c
Nitesh Kant <nitesh1706@gmail.com>
2016-06-29 17:23:09 -0700

    Statically create `TimeoutException`.
    #### Problem
    
    If a host is latent and most of the requests happen to timeout, creating a new `TimeoutException` for every timeout produces garbage and wastes CPU fetching the stack, which is useless.
    
    #### Solution
    
    Create a static instance of `TimeoutException` and fill it with empty stack.
    
    #### Result
    
    Less garbage, less work, more happiness.

--- reactivesocket-core/src/main/java/io/reactivesocket/internal/Publishers.java
+++ reactivesocket-core/src/main/java/io/reactivesocket/internal/Publishers.java
@@ -1,3 +1,16 @@
+/*
+ * Copyright 2016 Netflix, Inc.
+ * <p>
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ *  the License. You may obtain a copy of the License at
+ *  <p>
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *  <p>
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ *  an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ *  specific language governing permissions and limitations under the License.
+ */
+
 package io.reactivesocket.internal;
 
 import org.reactivestreams.Publisher;
@@ -15,6 +28,15 @@ import java.util.function.Function;
  * A set of utility functions for applying function composition over {@link Publisher}s.
  */
 public final class Publishers {
+
+    @SuppressWarnings("ThrowableInstanceNeverThrown")
+    public static final TimeoutException TIMEOUT_EXCEPTION = new TimeoutException();
+
+    public static final StackTraceElement[] EMPTY_STACK = new StackTraceElement[0];
+
+    static {
+        TIMEOUT_EXCEPTION.setStackTrace(EMPTY_STACK);
+    }
 
     private Publishers() {
         // No instances.
@@ -89,7 +111,7 @@ public final class Publishers {
                             _cancel = !emitted;
                         }
                         if (_cancel) {
-                            onError(new TimeoutException());
+                            onError(TIMEOUT_EXCEPTION);
                             cancel();
                         }
                     });
