9e590e278171955020854b9c8aa7ecc957d2e7b8
Martin Zhu <mjzhu@fb.com>
2016-07-29 13:43:54 -0700

    updated run.sh to automatically find latest version

--- reactivesocket-tck-drivers/run.sh
+++ reactivesocket-tck-drivers/run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-java -cp build/libs/reactivesocket-tck-drivers-0.2.3-SNAPSHOT.jar io.reactivesocket.tckdrivers.main.Main "$@"
+LATEST_VERSION=$(find build/libs/ | sort -r | head -1)
+
+echo "running latest version $LATEST_VERSION"
+
+java -cp "build/libs/$LATEST_VERSION" io.reactivesocket.tckdrivers.main.Main "$@"
