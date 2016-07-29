9a027dff0610c29a204147676880b1f30cd63088
xytosis <martin_zhu@brown.edu>
2016-07-29 02:19:34 -0400

    integrating tck driver (#153)

---
+++ reactivesocket-tck-drivers/src/main/java/io/reactivesocket/tckdrivers/main/Main.java
@@ -0,0 +1,48 @@
+package io.reactivesocket.tckdrivers.main;
+
+import io.airlift.airline.Command;
+import io.airlift.airline.Option;
+import io.airlift.airline.SingleCommand;
+import io.reactivesocket.tckdrivers.client.JavaTCPClient;
+import io.reactivesocket.tckdrivers.server.JavaTCPServer;
+
+/**
+ * This class is used to run both the server and the client, depending on the options given
+ */
+@Command(name = "reactivesocket-driver", description = "This runs the client and servers that use the driver")
+public class Main {
+
+    @Option(name = "--debug", description = "set if you want frame level output")
+    public static boolean debug;
+
+    @Option(name = "--server", description = "set if you want to run the server")
+    public static boolean server;
+
+    @Option(name = "--client", description = "set if you want to run the client")
+    public static boolean client;
+
+    @Option(name = "--host", description = "The host to connect to for the client")
+    public static String host;
+
+    @Option(name = "--port", description = "The port")
+    public static int port;
+
+    @Option(name = "--file", description = "The script file to parse, make sure to give the client and server the" +
+            "correct files")
+    public static String file;
+
+    public static void main(String[] args) {
+        SingleCommand<Main> cmd = SingleCommand.singleCommand(Main.class);
+        cmd.parse(args);
+        if (server) {
+            JavaTCPServer.run(file, port);
+        } else if (client) {
+            try {
+                JavaTCPClient.run(file, host, port, debug);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
---
+++ reactivesocket-tck-drivers/src/main/java/io/reactivesocket/tckdrivers/server/JavaTCPServer.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2016 Facebook, Inc.
+ * <p>
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ *  the License. You may obtain a copy of the License at
+ *  <p>
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *  <p>
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ *  an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ *  specific language governing permissions and limitations under the License.
+ */
+
+package io.reactivesocket.tckdrivers.server;
+
+import io.reactivesocket.transport.tcp.server.TcpReactiveSocketServer;
+
+/**
+ * An example of how to run the JavaServerDriver using the ReactiveSocket server creation tool in Java.
+ */
+public class JavaTCPServer {
+
+    public static void run(String realfile, int port) {
+
+        String file = "reactivesocket-tck-drivers/src/main/test/resources/servertest$.txt";
+
+        if (realfile != null) {
+            file = realfile;
+        }
+
+        JavaServerDriver jsd =
+                new JavaServerDriver(file);
+
+        TcpReactiveSocketServer.create(port)
+                .start((setupPayload, reactiveSocket) -> {
+                    // create request handler
+                    return jsd.parse();
+                }).awaitShutdown();
+
+
+    }
+
+}
