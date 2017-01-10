f8760c6210e786d5fc3c4d3cd86c5086870da0eb
Ben Christensen <benjchristensen@gmail.com>
2017-01-09 21:14:11 -0800

    Update buildViaTravis.sh

--- gradle/buildViaTravis.sh
+++ gradle/buildViaTravis.sh
@@ -14,4 +14,3 @@ else
   echo -e 'WARN: Should not be here => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']  Pull Request ['$TRAVIS_PULL_REQUEST']'
   ./gradlew -Prelease.useLastTag=true build
 fi
-
