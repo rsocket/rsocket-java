2938fc7b842c59c62a977bee9864b227f4b707bf
Yuri Schimke <yschimke@fb.com>
2017-05-06 17:01:20 +0100

    concurrent test

--- rsocket-examples/src/test/java/io/rsocket/integration/TcpIntegrationTest.java
+++ rsocket-examples/src/test/java/io/rsocket/integration/TcpIntegrationTest.java
@@ -1,6 +1,5 @@
 package io.rsocket.integration;
 
-import com.google.common.collect.Lists;
 import io.rsocket.AbstractRSocket;
 import io.rsocket.Payload;
 import io.rsocket.RSocket;
@@ -14,154 +13,174 @@ import io.rsocket.transport.netty.client.TcpTransportClient;
 import io.rsocket.transport.netty.server.TcpTransportServer;
 import io.rsocket.util.PayloadImpl;
 import io.rsocket.util.RSocketProxy;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CountDownLatch;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import reactor.core.publisher.DirectProcessor;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.publisher.UnicastProcessor;
+import reactor.core.scheduler.Schedulers;
 import reactor.ipc.netty.tcp.TcpClient;
 import reactor.ipc.netty.tcp.TcpServer;
-
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Queue;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
 public class TcpIntegrationTest {
-    private AbstractRSocket handler = new AbstractRSocket() {
+  private AbstractRSocket handler = new AbstractRSocket() {
+  };
+
+  private TransportServer.StartedServer server;
+
+  @Before
+  public void startup() {
+    server = RSocketServer.create(
+        TcpTransportServer.create(TcpServer.create()))
+        .start(
+            (setup, sendingSocket) -> new DisabledLeaseAcceptingSocket(new RSocketProxy(handler)));
+  }
+
+  private RSocket buildClient() {
+    return RSocketClient.create(
+        TcpTransportClient.create(TcpClient.create(server.getServerPort())),
+        SetupProvider.keepAlive(KeepAliveProvider.never()).disableLease())
+        .connect().block();
+  }
+
+  @After
+  public void cleanup() {
+    server.shutdown();
+  }
+
+  @Test(timeout = 2_000L)
+  public void testCompleteWithoutNext() throws InterruptedException {
+    handler = new AbstractRSocket() {
+      @Override
+      public Flux<Payload> requestStream(Payload payload) {
+        return Flux.empty();
+      }
     };
 
-    private TransportServer.StartedServer server;
+    RSocket client = buildClient();
 
-    @Before
-    public void startup() {
-        server = RSocketServer.create(
-                TcpTransportServer.create(TcpServer.create()))
-                .start((setup, sendingSocket) -> new DisabledLeaseAcceptingSocket(new RSocketProxy(handler)));
-    }
+    Boolean hasElements =
+        client.requestStream(new PayloadImpl("REQUEST", "META")).hasElements().block();
 
-    private RSocket buildClient() {
-        return RSocketClient.create(
-                TcpTransportClient.create(TcpClient.create(server.getServerPort())),
-                SetupProvider.keepAlive(KeepAliveProvider.never()).disableLease())
-                .connect().block();
-    }
+    assertFalse(hasElements);
+  }
 
-    @After
-    public void cleanup() {
-        server.shutdown();
-    }
+  @Test(timeout = 2_000L)
+  public void testSingleStream() throws InterruptedException {
+    handler = new AbstractRSocket() {
+      @Override
+      public Flux<Payload> requestStream(Payload payload) {
+        return Flux.just(new PayloadImpl("RESPONSE", "METADATA"));
+      }
+    };
 
-    @Test(timeout = 2_000L)
-    public void testCompleteWithoutNext() throws InterruptedException {
-        handler = new AbstractRSocket() {
-            @Override
-            public Flux<Payload> requestStream(Payload payload) {
-                return Flux.empty();
-            }
-        };
+    RSocket client = buildClient();
 
-        RSocket client = buildClient();
+    Payload result = client.requestStream(new PayloadImpl("REQUEST", "META")).blockLast();
 
-        Boolean hasElements = client.requestStream(new PayloadImpl("REQUEST", "META")).hasElements().block();
+    assertEquals("RESPONSE", StandardCharsets.UTF_8.decode(result.getData()).toString());
+  }
 
-        assertFalse(hasElements);
-    }
+  @Test(timeout = 2_000L)
+  public void testZeroPayload() throws InterruptedException {
+    handler = new AbstractRSocket() {
+      @Override
+      public Flux<Payload> requestStream(Payload payload) {
+        return Flux.just(PayloadImpl.EMPTY);
+      }
+    };
 
-    @Test(timeout = 2_000L)
-    public void testSingleStream() throws InterruptedException {
-        handler = new AbstractRSocket() {
-            @Override
-            public Flux<Payload> requestStream(Payload payload) {
-                return Flux.just(new PayloadImpl("RESPONSE", "METADATA"));
-            }
-        };
+    RSocket client = buildClient();
 
-        RSocket client = buildClient();
+    Payload result = client.requestStream(new PayloadImpl("REQUEST", "META")).blockFirst();
 
-        Payload result = client.requestStream(new PayloadImpl("REQUEST", "META")).blockLast();
+    assertEquals("", StandardCharsets.UTF_8.decode(result.getData()).toString());
+  }
 
-        assertEquals("RESPONSE", StandardCharsets.UTF_8.decode(result.getData()).toString());
-    }
+  @Test(timeout = 2_000L)
+  public void testRequestResponseErrors() throws InterruptedException {
+    handler = new AbstractRSocket() {
+      boolean first = true;
 
-    @Test(timeout = 2_000L)
-    public void testZeroPayload() throws InterruptedException {
-        handler = new AbstractRSocket() {
-            @Override
-            public Flux<Payload> requestStream(Payload payload) {
-                return Flux.just(PayloadImpl.EMPTY);
-            }
-        };
+      @Override
+      public Mono<Payload> requestResponse(Payload payload) {
+        if (first) {
+          first = false;
+          return Mono.error(new RuntimeException("EX"));
+        } else {
+          return Mono.just(new PayloadImpl("SUCCESS"));
+        }
+      }
+    };
 
-        RSocket client = buildClient();
+    RSocket client = buildClient();
 
-        Payload result = client.requestStream(new PayloadImpl("REQUEST", "META")).blockFirst();
+    Payload response1 = client.requestResponse(new PayloadImpl("REQUEST", "META"))
+        .onErrorReturn(new PayloadImpl("ERROR"))
+        .block();
+    Payload response2 = client.requestResponse(new PayloadImpl("REQUEST", "META"))
+        .onErrorReturn(new PayloadImpl("ERROR"))
+        .block();
 
-        assertEquals("", StandardCharsets.UTF_8.decode(result.getData()).toString());
-    }
+    assertEquals("ERROR", StandardCharsets.UTF_8.decode(response1.getData()).toString());
+    assertEquals("SUCCESS", StandardCharsets.UTF_8.decode(response2.getData()).toString());
+  }
 
-    @Test(timeout = 2_000L)
-    public void testRequestResponseErrors() throws InterruptedException {
-        handler = new AbstractRSocket() {
-            boolean first = true;
+  @Test(timeout = 2_000L)
+  public void testTwoConcurrentStreams() throws InterruptedException {
+    // Two concurrent streams
+    // Stream 1 is a single response
+    // Stream 2 is a single response, then completed by the server
+    // Stream 1 is then cancelled by the client
 
-            @Override
-            public Mono<Payload> requestResponse(Payload payload) {
-                if (first) {
-                    first = false;
-                    return Mono.error(new RuntimeException("EX"));
-                } else {
-                    return Mono.just(new PayloadImpl("SUCCESS"));
-                }
-            }
-        };
+    ConcurrentHashMap<String, UnicastProcessor<Payload>> map = new ConcurrentHashMap<>();
+    UnicastProcessor<Payload> processor1 = UnicastProcessor.create();
+    map.put("REQUEST1", processor1);
+    UnicastProcessor<Payload> processor2 = UnicastProcessor.create();
+    map.put("REQUEST2", processor2);
 
-        RSocket client = buildClient();
+    handler = new AbstractRSocket() {
+      @Override
+      public Flux<Payload> requestStream(Payload payload) {
+        String s = StandardCharsets.UTF_8.decode(payload.getData()).toString();
+        return map.get(s);
+      }
+    };
 
-        Payload response1 = client.requestResponse(new PayloadImpl("REQUEST", "META")).onErrorReturn(new PayloadImpl("ERROR")).block();
-        Payload response2 = client.requestResponse(new PayloadImpl("REQUEST", "META")).onErrorReturn(new PayloadImpl("ERROR")).block();
+    RSocket client = buildClient();
 
-        assertEquals("ERROR", StandardCharsets.UTF_8.decode(response1.getData()).toString());
-        assertEquals("SUCCESS", StandardCharsets.UTF_8.decode(response2.getData()).toString());
-    }
+    Flux<Payload> response1 = client.requestStream(new PayloadImpl("REQUEST1"));
+    Flux<Payload> response2 = client.requestStream(new PayloadImpl("REQUEST2"));
 
+    CountDownLatch nextCountdown = new CountDownLatch(2);
+    CountDownLatch completeCountdown = new CountDownLatch(2);
 
-    @Test(timeout = 2_000L)
-    public void testTwoConcurrentStreams() throws InterruptedException {
-        // Two concurrent streams
-        // Stream 1 is a single response
-        // Stream 2 is a single response, then completed by the server
-        // Stream 1 is then cancelled by the client
+    response1
+        .subscribeOn(Schedulers.newSingle("1"))
+        .subscribe(c -> nextCountdown.countDown(), t -> {
+        }, completeCountdown::countDown);
 
-        Queue<DirectProcessor<Payload>> streamResults = Lists.newLinkedList();
-        DirectProcessor<Payload> processor1 = DirectProcessor.create();
-        streamResults.add(processor1);
-        DirectProcessor<Payload> processor2 = DirectProcessor.create();
-        streamResults.add(processor2);
+    response2
+        .subscribeOn(Schedulers.newSingle("2"))
+        .subscribe(c -> nextCountdown.countDown(), t -> {
+        }, completeCountdown::countDown);
 
-        handler = new AbstractRSocket() {
-            @Override
-            public Flux<Payload> requestStream(Payload payload) {
-                return streamResults.remove();
-            }
-        };
+    processor1.onNext(new PayloadImpl("RESPONSE1A"));
+    processor2.onNext(new PayloadImpl("RESPONSE2A"));
 
-        RSocket client = buildClient();
+    nextCountdown.await();
 
-        Flux<Payload> response1 = client.requestStream(new PayloadImpl("REQUEST1"));
-        Flux<Payload> response2 = client.requestStream(new PayloadImpl("REQUEST2"));
+    processor1.onComplete();
+    processor2.onComplete();
 
-        processor1.onNext(new PayloadImpl("RESPONSE1A"));
-        // TODO comment out this line
-        processor1.onComplete();
-        processor2.onNext(new PayloadImpl("RESPONSE2A"));
-        processor2.onComplete();
-
-        Payload r2a = response2.blockFirst();
-        Payload r1a = response1.blockFirst();
-    }
+    completeCountdown.await();
+  }
 }
