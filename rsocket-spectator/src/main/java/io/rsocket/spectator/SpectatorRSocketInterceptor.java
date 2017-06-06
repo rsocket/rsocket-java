4744abd919238a8770ff7a0575db2a39e442b3c3
Yuri Schimke <yschimke@fb.com>
2017-06-06 22:21:05 +0100

    format one package

--- rsocket-spectator/src/main/java/io/rsocket/spectator/SpectatorRSocketInterceptor.java
+++ rsocket-spectator/src/main/java/io/rsocket/spectator/SpectatorRSocketInterceptor.java
@@ -5,28 +5,24 @@ import io.rsocket.Plugins;
 import io.rsocket.RSocket;
 import reactor.core.publisher.Mono;
 
-/**
- * Interceptor that wraps a {@link RSocket} with a {@link SpectatorRSocket}
- */
+/** Interceptor that wraps a {@link RSocket} with a {@link SpectatorRSocket} */
 public class SpectatorRSocketInterceptor implements Plugins.RSocketInterceptor {
-    private static final String[] EMPTY = new String[0];
-    private final Registry registry;
-    private final String[] tags;
+  private static final String[] EMPTY = new String[0];
+  private final Registry registry;
+  private final String[] tags;
 
-    public SpectatorRSocketInterceptor(Registry registry, String... tags) {
-        this.registry = registry;
-        this.tags = tags;
-    }
+  public SpectatorRSocketInterceptor(Registry registry, String... tags) {
+    this.registry = registry;
+    this.tags = tags;
+  }
 
-    public SpectatorRSocketInterceptor(Registry registry) {
-        this(registry, EMPTY);
-    }
+  public SpectatorRSocketInterceptor(Registry registry) {
+    this(registry, EMPTY);
+  }
 
-    @Override
-    public Mono<RSocket> apply(RSocket reactiveSocket) {
+  @Override
+  public Mono<RSocket> apply(RSocket reactiveSocket) {
 
-        return Mono.just(new SpectatorRSocket(registry, reactiveSocket, tags));
-
-
-    }
+    return Mono.just(new SpectatorRSocket(registry, reactiveSocket, tags));
+  }
 }
