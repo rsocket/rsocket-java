1851bd600a4e10eb3804234ff655f8b951fa0d68
Yuri Schimke <yuri@schimke.ee>
2017-10-08 18:55:32 +0100

    move to rsocket-tck

--- rsocket-tck-drivers/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-LATEST_VERSION=$(ls rsocket-tck-drivers/build/libs/rsocket-tck-drivers-*-SNAPSHOT.jar | sort -r | head -1)
-
-java -cp "$LATEST_VERSION" io.rsocket.tckdrivers.main.Main "$@"
