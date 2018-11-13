b2023ffb938e22d7cd24c9ffbbe4d8f4dd517046
Robert Roeser <robert@netifi.io>
2018-11-13 12:46:29 -0500

    Switching FrameType flags back to bits (#548)
    * switching frame type flag back to bits so we don't have to traverse an array when looking up flags
    
    Signed-off-by: Robert Roeser <rroeserr@gmail.com>
    
    * jmh test
    
    Signed-off-by: Robert Roeser <rroeserr@gmail.com>
    
    * Added an EMPTY flag
    
    Signed-off-by: Robert Roeser <rroeserr@gmail.com>
    Signed-off-by: Robert Roeser <rroeserr@gmail.com>

---
+++ rsocket-core/src/jmh/java/io/rsocket/framing/FrameTypePerf.java
@@ -0,0 +1,38 @@
+package io.rsocket.framing;
+
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.infra.Blackhole;
+
+@BenchmarkMode(Mode.Throughput)
+@Fork(
+  value = 1 // , jvmArgsAppend = {"-Dio.netty.leakDetection.level=advanced"}
+)
+@Warmup(iterations = 10)
+@Measurement(iterations = 10)
+@State(Scope.Thread)
+public class FrameTypePerf {
+  @Benchmark
+  public void lookup(Input input) {
+    FrameType frameType = input.frameType;
+    boolean b =
+        frameType.canHaveData()
+            && frameType.canHaveMetadata()
+            && frameType.isFragmentable()
+            && frameType.isRequestType()
+            && frameType.hasInitialRequestN();
+
+    input.bh.consume(b);
+  }
+
+  @State(Scope.Benchmark)
+  public static class Input {
+    Blackhole bh;
+    FrameType frameType;
+
+    @Setup
+    public void setup(Blackhole bh) {
+      this.bh = bh;
+      this.frameType = FrameType.REQUEST_RESPONSE;
+    }
+  }
+}
