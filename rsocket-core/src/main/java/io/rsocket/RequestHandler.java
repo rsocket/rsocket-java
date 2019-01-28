9c5ac91b11e9c7bafc4c5f0c3b91ebbc67bb148f
Robert Roeser <rroeserr@gmail.com>
2019-01-28 11:33:14 -0800

    updated javadoc
    Signed-off-by: Robert Roeser <rroeserr@gmail.com>

--- rsocket-core/src/main/java/io/rsocket/RequestHandler.java
+++ rsocket-core/src/main/java/io/rsocket/RequestHandler.java
@@ -4,12 +4,16 @@ import org.reactivestreams.Publisher;
 import reactor.core.publisher.Flux;
 
 /**
- * Extends the {@link RSocket} that allows an implementor to peek at the first request payload of a channel.
+ * Extends the {@link RSocket} that allows an implementer to peek at the first request payload of a
+ * channel.
  */
 public interface RequestHandler extends RSocket {
   /**
-   * Implement this method to peak at the first payload of the incoming request stream
-   * @param payload First payload in the stream
+   * Implement this method to peak at the first payload of the incoming request stream without
+   * having to subscribe to Publish&lt;Payload&gt; payloads
+   *
+   * @param payload First payload in the stream - this is the same payload as the first payload in
+   *     Publisher&lt;Payload&gt; payloads
    * @param payloads Stream of request payloads.
    * @return Stream of response payloads.
    */
