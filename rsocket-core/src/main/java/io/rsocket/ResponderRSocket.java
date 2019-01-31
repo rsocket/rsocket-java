21c39bdaa60c6a916e3288048b268f2b756dd5e7
Robert Roeser <robert@netifi.io>
2019-01-31 10:27:41 -0800

    Adds specific requestChannel(Payload, Publisher<Payload>)  (#572)
    * optimize request channel to let you peak at the first item in the requestChannel
    
    Signed-off-by: Robert Roeser <rroeserr@gmail.com>
    
    * updated javadoc
    
    Signed-off-by: Robert Roeser <rroeserr@gmail.com>
    
    * updates
    
    Signed-off-by: Robert Roeser <rroeserr@gmail.com>
    
    * call requestChannelWith 2 arguments
    
    Signed-off-by: Robert Roeser <rroeserr@gmail.com>

---
+++ rsocket-core/src/main/java/io/rsocket/ResponderRSocket.java
@@ -0,0 +1,23 @@
+package io.rsocket;
+
+import org.reactivestreams.Publisher;
+import reactor.core.publisher.Flux;
+
+/**
+ * Extends the {@link RSocket} that allows an implementer to peek at the first request payload of a
+ * channel.
+ */
+public interface ResponderRSocket extends RSocket {
+  /**
+   * Implement this method to peak at the first payload of the incoming request stream without
+   * having to subscribe to Publish&lt;Payload&gt; payloads
+   *
+   * @param payload First payload in the stream - this is the same payload as the first payload in
+   *     Publisher&lt;Payload&gt; payloads
+   * @param payloads Stream of request payloads.
+   * @return Stream of response payloads.
+   */
+  default Flux<Payload> requestChannel(Payload payload, Publisher<Payload> payloads) {
+    return requestChannel(payloads);
+  }
+}
