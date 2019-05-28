9cd25e55efc62705cc93f07dd7d70546145fe66d
Oleh Dokuka <shadowgun@i.ua>
2019-05-28 19:24:42 +0300

    partially ignore test
    Signed-off-by: Oleh Dokuka <shadowgun@i.ua>

--- rsocket-test/src/main/java/io/rsocket/test/TransportTest.java
+++ rsocket-test/src/main/java/io/rsocket/test/TransportTest.java
@@ -115,19 +115,19 @@ public interface TransportTest {
         .verify(getTimeout());
   }
 
-  @DisplayName("makes 1 requestChannel request with 200,000 payloads")
-  @Test
-  @Ignore
-  default void requestChannel200_000() {
-    Flux<Payload> payloads = Flux.range(0, 200_000).map(this::createTestPayload);
-
-    getClient()
-        .requestChannel(payloads)
-        .as(StepVerifier::create)
-        .expectNextCount(200_000)
-        .expectComplete()
-        .verify(getTimeout());
-  }
+//  @DisplayName("makes 1 requestChannel request with 200,000 payloads")
+//  @Test
+//  @Ignore
+//  default void requestChannel200_000() {
+//    Flux<Payload> payloads = Flux.range(0, 200_000).map(this::createTestPayload);
+//
+//    getClient()
+//        .requestChannel(payloads)
+//        .as(StepVerifier::create)
+//        .expectNextCount(200_000)
+//        .expectComplete()
+//        .verify(getTimeout());
+//  }
 
   @DisplayName("makes 1 requestChannel request with 20,000 payloads")
   @Test
