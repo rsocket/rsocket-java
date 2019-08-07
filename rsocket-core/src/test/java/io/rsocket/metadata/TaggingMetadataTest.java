6115bbc177f0cf4f9ea6784bd1f67da8ad68f237
linux_china <libing.chen@gmail.com>
2019-08-06 19:51:07 -0700

    Tagging metadata test
    Signed-off-by: linux_china <libing.chen@gmail.com>

---
+++ rsocket-core/src/test/java/io/rsocket/metadata/TaggingMetadataTest.java
@@ -0,0 +1,35 @@
+package io.rsocket.metadata;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Tagging metadata test
+ *
+ * @author linux_china
+ */
+public class TaggingMetadataTest {
+
+    @Test
+    public void testParseTags() {
+        List<String> tags = Arrays.asList("ws://localhost:8080/rsocket", String.join("", Collections.nCopies(129, "x")));
+        TaggingMetadata taggingMetadata = new TaggingMetadata("message/x.rsocket.routing.v0", tags);
+        TaggingMetadata taggingMetadataCopy = new TaggingMetadata("message/x.rsocket.routing.v0", taggingMetadata.getContent());
+        assertThat(tags).containsExactlyElementsOf(taggingMetadataCopy.stream().collect(Collectors.toList()));
+    }
+
+    @Test
+    public void testEmptyTagAndOverLengthTag() {
+        List<String> tags = Arrays.asList("ws://localhost:8080/rsocket", "", String.join("", Collections.nCopies(256, "x")));
+        TaggingMetadata taggingMetadata = new TaggingMetadata("message/x.rsocket.routing.v0", tags);
+        TaggingMetadata taggingMetadataCopy = new TaggingMetadata("message/x.rsocket.routing.v0", taggingMetadata.getContent());
+        assertThat(tags.subList(0, 1)).containsExactlyElementsOf(taggingMetadataCopy.stream().collect(Collectors.toList()));
+    }
+
+}
