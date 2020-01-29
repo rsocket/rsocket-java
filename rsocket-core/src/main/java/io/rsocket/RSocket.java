047a62e1c81f34bcd2bcaf8e2ad4cfed9aa52d83
linux_china <libing.chen@gmail.com>
2020-01-29 10:13:49 -0800

    extends AttributesContext
    Signed-off-by: linux_china <libing.chen@gmail.com>

--- rsocket-core/src/main/java/io/rsocket/RSocket.java
+++ rsocket-core/src/main/java/io/rsocket/RSocket.java
@@ -24,7 +24,7 @@ import reactor.core.publisher.Mono;
  * A contract providing different interaction models for <a
  * href="https://github.com/RSocket/reactivesocket/blob/master/Protocol.md">RSocket protocol</a>.
  */
-public interface RSocket extends Availability, Closeable {
+public interface RSocket extends Availability, Closeable, AttributesContext {
 
   /**
    * Fire and Forget interaction model of {@code RSocket}.
