beb0d596591ef6ce6602d324cfd086b7b4c7a31b
Oleh Dokuka <shadowgun@i.ua>
2020-05-10 20:55:36 +0300

    provides example polish
    Signed-off-by: Oleh Dokuka <shadowgun@i.ua>

--- rsocket-examples/src/main/java/io/rsocket/examples/transport/tcp/plugins/LimitRateInterceptorExample.java
+++ rsocket-examples/src/main/java/io/rsocket/examples/transport/tcp/plugins/LimitRateInterceptorExample.java
@@ -2,6 +2,7 @@ package io.rsocket.examples.transport.tcp.plugins;
 
 import io.rsocket.Payload;
 import io.rsocket.RSocket;
+import io.rsocket.SocketAcceptor;
 import io.rsocket.core.RSocketConnector;
 import io.rsocket.core.RSocketServer;
 import io.rsocket.examples.transport.tcp.stream.StreamingClient;
@@ -17,36 +18,34 @@ import org.reactivestreams.Publisher;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 import reactor.util.concurrent.Queues;
 
-public class LimitRateInterceptoreExample {
+public class LimitRateInterceptorExample {
 
   private static final Logger logger = LoggerFactory.getLogger(StreamingClient.class);
 
   public static void main(String[] args) {
     BlockingQueue<String> requests = new ArrayBlockingQueue<>(100);
     RSocketServer.create(
-            (sp, sr) ->
-                Mono.just(
-                    new RSocket() {
-                      @Override
-                      public Flux<Payload> requestStream(Payload payload) {
-                        return Flux.interval(Duration.ofMillis(100))
-                            .doOnRequest(e -> requests.add("Responder requestN(" + e + ")"))
-                            .map(aLong -> DefaultPayload.create("Interval: " + aLong));
-                      }
+            SocketAcceptor.with(
+                new RSocket() {
+                  @Override
+                  public Flux<Payload> requestStream(Payload payload) {
+                    return Flux.interval(Duration.ofMillis(100))
+                        .doOnRequest(e -> requests.add("Responder requestN(" + e + ")"))
+                        .map(aLong -> DefaultPayload.create("Interval: " + aLong));
+                  }
 
-                      @Override
-                      public Flux<Payload> requestChannel(Publisher<Payload> payloads) {
-                        return Flux.from(payloads)
-                            .doOnRequest(e -> requests.add("Responder requestN(" + e + ")"));
-                      }
-                    }))
+                  @Override
+                  public Flux<Payload> requestChannel(Publisher<Payload> payloads) {
+                    return Flux.from(payloads)
+                        .doOnRequest(e -> requests.add("Responder requestN(" + e + ")"));
+                  }
+                }))
         .interceptors(
             ir ->
-                ir.forRequester((RSocketInterceptor) r -> new LimitRateRSocket(r, true))
-                    .forResponder((RSocketInterceptor) r -> new LimitRateRSocket(r, false)))
+                ir.forRequester(LimitRateInterceptor.forRequester())
+                    .forResponder(LimitRateInterceptor.forResponder()))
         .bind(TcpServerTransport.create("localhost", 7000))
         .subscribe();
 
@@ -54,8 +53,8 @@ public class LimitRateInterceptoreExample {
         RSocketConnector.create()
             .interceptors(
                 ir ->
-                    ir.forRequester((RSocketInterceptor) r -> new LimitRateRSocket(r, true))
-                        .forResponder((RSocketInterceptor) r -> new LimitRateRSocket(r, false)))
+                    ir.forRequester(LimitRateInterceptor.forRequester())
+                        .forResponder(LimitRateInterceptor.forResponder()))
             .connect(TcpClientTransport.create("localhost", 7000))
             .block();
 
@@ -94,19 +93,53 @@ public class LimitRateInterceptoreExample {
     requests.forEach(request -> logger.debug("Requested : {}", request));
   }
 
+  static class LimitRateInterceptor implements RSocketInterceptor {
+
+    final boolean requesterSide;
+    final int highTide;
+    final int lowTide;
+
+    LimitRateInterceptor(boolean requesterSide, int highTide, int lowTide) {
+      this.requesterSide = requesterSide;
+      this.highTide = highTide;
+      this.lowTide = lowTide;
+    }
+
+    @Override
+    public RSocket apply(RSocket socket) {
+      return new LimitRateRSocket(socket, requesterSide, highTide, lowTide);
+    }
+
+    public static LimitRateInterceptor forRequester() {
+      return forRequester(Queues.SMALL_BUFFER_SIZE);
+    }
+
+    public static LimitRateInterceptor forRequester(int limit) {
+      return forRequester(limit, limit);
+    }
+
+    public static LimitRateInterceptor forRequester(int highTide, int lowTide) {
+      return new LimitRateInterceptor(true, highTide, lowTide);
+    }
+
+    public static LimitRateInterceptor forResponder() {
+      return forRequester(Queues.SMALL_BUFFER_SIZE);
+    }
+
+    public static LimitRateInterceptor forResponder(int limit) {
+      return forRequester(limit, limit);
+    }
+
+    public static LimitRateInterceptor forResponder(int highTide, int lowTide) {
+      return new LimitRateInterceptor(false, highTide, lowTide);
+    }
+  }
+
   static class LimitRateRSocket extends RSocketProxy {
 
-    private final boolean requesterSide;
-    private final int highTide;
-    private final int lowTide;
-
-    public LimitRateRSocket(RSocket source, boolean requesterSide) {
-      this(source, requesterSide, Queues.SMALL_BUFFER_SIZE);
-    }
-
-    public LimitRateRSocket(RSocket source, boolean requesterSide, int highTide) {
-      this(source, requesterSide, highTide, highTide);
-    }
+    final boolean requesterSide;
+    final int highTide;
+    final int lowTide;
 
     public LimitRateRSocket(RSocket source, boolean requesterSide, int highTide, int lowTide) {
       super(source);
