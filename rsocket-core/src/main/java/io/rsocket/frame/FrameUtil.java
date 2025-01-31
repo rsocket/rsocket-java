d28d093aaef21bca01d8de12c2fb9be8492ed982
rstoyanchev <rossen.stoyanchev@broadcom.com>
2025-01-31 11:53:11 +0000

    Log data in KEEPALIVE frame
    Fixes gh-1114
    
    Signed-off-by: rstoyanchev <rossen.stoyanchev@broadcom.com>

--- rsocket-core/src/main/java/io/rsocket/frame/FrameUtil.java
+++ rsocket-core/src/main/java/io/rsocket/frame/FrameUtil.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2015-2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.rsocket.frame;
 
 import io.netty.buffer.ByteBuf;
@@ -99,8 +114,9 @@ public class FrameUtil {
       case REQUEST_CHANNEL:
         data = RequestChannelFrameCodec.data(frame);
         break;
-        // Payload and synthetic types
+        // Payload, KeepAlive and synthetic types
       case PAYLOAD:
+      case KEEPALIVE:
       case NEXT:
       case NEXT_COMPLETE:
       case COMPLETE:
